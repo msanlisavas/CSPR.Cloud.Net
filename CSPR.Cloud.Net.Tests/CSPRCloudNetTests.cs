09eb5a5b6eee2c94b825690062f5dde552a106b8
Murat Şanlısavaş <muratsanlisavas@gmail.com>
2024-02-04 15:21:30 +0300

    test

--- CSPR.Cloud.Net.Tests/CSPRCloudNetTests.cs
+++ CSPR.Cloud.Net.Tests/CSPRCloudNetTests.cs
@@ -12,10 +12,12 @@ namespace CSPR.Cloud.Net.Tests
     {
         private readonly CasperCloudRestClient _restClient;
         private readonly string _testPublicKey = "012d58e05b2057a84115709e0a6ccf000c6a83b4e8dfa389a680c1ab001864f1f2";
-        private readonly string _testAccountHash = "68bae9382be8706fa9533f33562eb1d58a879e42ccd1e8daf7368b17850304dc";
+        private readonly string _test2PublicKey = "012d58e05b2057a84115709e0a6ccf000c6a83b4e8dfa389a680c1ab001864f1f2";
+        private readonly string _testAccountHash = "0106ca7c39cd272dbf21a86eeb3b36b7c26e2e9b94af64292419f7862936bca2ca";
+        private readonly string _test2AccountHash = "fa12d2dd5547714f8c2754d418aa8c9d59dc88780350cb4254d622e2d4ef7e69";
         public CSPRCloudNetTests()
         {
-            _restClient = new CasperCloudRestClient(new CasperCloudClientConfig("55f79117-fc4d-4d60-9956-65423f39a06a"));
+            _restClient = new CasperCloudRestClient(new CasperCloudClientConfig("55f79117-fc4d-4d60-9956-65423f39a06a")); // test key
         }
 
 
@@ -101,17 +103,49 @@ namespace CSPR.Cloud.Net.Tests
         {
             var parameters = new AccountsRequestParameters
             {
-
                 QueryParameters = new AccountsQueryParameters
                 {
                     AccountHashes = new List<string>
                     {
                         _testAccountHash
                     }
+                },
+                Sorting = new AccountsSorting
+                {
+                    OrderBy = new List<string>
+                    {
+                        "balance"
+                    },
+                    SortType = SortType.Ascending
                 }
             };
             var result = await _restClient.Testnet.GetAccountsAsync(parameters);
             Assert.True(result.Data[0].PublicKey == _testPublicKey);
+        }
+        [Fact]
+        public async Task GetAccountsAsync_ReturnsMultipleAccountHashesData()
+        {
+            var parameters = new AccountsRequestParameters
+            {
+                QueryParameters = new AccountsQueryParameters
+                {
+                    AccountHashes = new List<string>
+                    {
+                        _testAccountHash,
+                        _test2AccountHash
+                    }
+                },
+                Sorting = new AccountsSorting
+                {
+                    OrderBy = new List<string>
+                    {
+                        "balance"
+                    },
+                    SortType = SortType.Ascending
+                }
+            };
+            var result = await _restClient.Testnet.GetAccountsAsync(parameters);
+            Assert.True(result.Data[0].PublicKey == _test2PublicKey && result.Data[1].PublicKey == _testPublicKey);
         }
         [Fact]
         public async Task GetAccountsAsync_ThrowsException_WithWrongOrderByParameters()
