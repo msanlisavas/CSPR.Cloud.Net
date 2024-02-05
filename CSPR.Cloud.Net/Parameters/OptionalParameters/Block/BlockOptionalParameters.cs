403382b55ebb0c2e3274bf642103807ff0c34f6b
Murat Şanlısavaş <muratsanlisavas@gmail.com>
2024-02-05 13:49:30 +0300

    block opt parameters

---
+++ CSPR.Cloud.Net/Parameters/OptionalParameters/Block/BlockOptionalParameters.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace CSPR.Cloud.Net.Parameters.OptionalParameters.Block
+{
+    public class BlockOptionalParameters
+    {
+        [JsonProperty("proposer_account_info")]
+        public bool ProposerAccountInfo { get; set; } = false;
+    }
+}
