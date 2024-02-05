3db84b99539336f39e180f4585a610e991820ef0
Murat Şanlısavaş <muratsanlisavas@gmail.com>
2024-02-05 13:54:50 +0300

    block class

---
+++ CSPR.Cloud.Net/Objects/Block/BlockData.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace CSPR.Cloud.Net.Objects.Block
+{
+    public class BlockData
+    {
+        [JsonProperty("block_height")]
+        public ulong? BlockHeight { get; set; }
+
+        [JsonProperty("block_hash")]
+        public string BlockHash { get; set; }
+
+        [JsonProperty("parent_block_hash")]
+        public string ParentBlockHash { get; set; }
+
+        [JsonProperty("state_root_hash")]
+        public string StateRootHash { get; set; }
+
+        [JsonProperty("era_id")]
+        public uint? EraId { get; set; }
+
+        [JsonProperty("proposer_public_key")]
+        public string ProposerPublicKey { get; set; }
+
+        [JsonProperty("native_transfers_number")]
+        public ushort? NativeTransfersNumber { get; set; }
+
+        [JsonProperty("contract_calls_number")]
+        public ushort? ContractCallsNumber { get; set; }
+
+        [JsonProperty("is_switch_block")]
+        public bool IsSwitchBlock { get; set; }
+
+        [JsonProperty("timestamp")]
+        public DateTime? Timestamp { get; set; }
+    }
+}
